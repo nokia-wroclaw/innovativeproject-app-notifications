30aba973f961bd2a6a5e63e17da989445afda9e2
Adam <adammaciak96@gmail.com>
2018-04-24 18:19:49 +0200

    cleaned unused overrides and added userID to notifications' JSONs

--- src/main/java/TwitterStreamReader.java
+++ src/main/java/TwitterStreamReader.java
@@ -8,10 +8,6 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -27,35 +23,37 @@ public final class TwitterStreamReader {
     private static String queueString;
 
     private static final UserStreamListener listener = new UserStreamListener() {
+
         @Override
         public void onStatus(Status status) {
-            System.out.println("onStatus @" + status.getUser().getScreenName() + " - " + status.getText());
-            String topic = "New status by " + status.getUser().getScreenName() + "!";
-            try {
-                generateNote(status, topic);
-            } catch (IOException|TimeoutException e) {
-                e.printStackTrace();
+            if (status.getText().contains("@Enfaude")) {
+                String topic = "New mention of you made by " + status.getUser().getScreenName() + "!";
+                try {
+                    generateNote(status, topic);
+                } catch (IOException | TimeoutException e) {
+                    e.printStackTrace();
+                }
             }
         }
 
         @Override
         public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
-//            System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
+
         }
 
         @Override
         public void onDeletionNotice(long directMessageId, long userId) {
-//            System.out.println("Got a direct message deletion notice id:" + directMessageId);
+
         }
 
         @Override
         public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-//            System.out.println("Got a track limitation notice:" + numberOfLimitedStatuses);
+//
         }
 
         @Override
         public void onScrubGeo(long userId, long upToStatusId) {
-//            System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+//
         }
 
         @Override
@@ -75,12 +73,6 @@ public final class TwitterStreamReader {
         @Override
         public void onFavorite(User source, User target, Status favoritedStatus) {
             String topic = "Tweet liked!";
-            /*System.out.println("onFavorite source:@"
-                    + source.getScreenName() + " target:@"
-                    + target.getScreenName() + " @"
-                    + favoritedStatus.getUser().getScreenName() + " - "
-                    + favoritedStatus.getText()
-                    + "   |Tweet's URL: https://twitter.com/statuses/" + favoritedStatus.getId());*/
             String message = favoritedStatus.getUser().getScreenName() + " has liked your tweet!";
             try {
                 generateNote(topic, message);
@@ -91,18 +83,11 @@ public final class TwitterStreamReader {
 
         @Override
         public void onUnfavorite(User source, User target, Status unfavoritedStatus) {
-            /*System.out.println("onUnFavorite source:@"
-                    + source.getScreenName() + " target:@"
-                    + target.getScreenName() + " @"
-                    + unfavoritedStatus.getUser().getScreenName()
-                    + " - " + unfavoritedStatus.getText());*/
+
         }
 
         @Override
         public void onFollow(User source, User followedUser) {
-            System.out.println("onFollow source:@"
-                    + source.getScreenName() + " target:@"
-                    + followedUser.getScreenName());
             String topic = "New follower!";
             String message = source.getScreenName() + " has followed " + followedUser.getScreenName() + "!";
             try {
@@ -114,15 +99,11 @@ public final class TwitterStreamReader {
 
         @Override
         public void onUnfollow(User source, User followedUser) {
-            /*System.out.println("onFollow source:@"
-                    + source.getScreenName() + " target:@"
-                    + followedUser.getScreenName());*/
+
         }
 
         @Override
         public void onDirectMessage(DirectMessage directMessage) {
-            System.out.println("onDirectMessage text:"
-                    + directMessage.getText());
             String topic = "New direct message!";
             String message = directMessage.getSenderScreenName() + " has sent a message to " + directMessage.getRecipientScreenName() + "!";
             try {
@@ -134,82 +115,62 @@ public final class TwitterStreamReader {
 
         @Override
         public void onUserListMemberAddition(User addedMember, User listOwner, UserList list) {
-            /*System.out.println("onUserListMemberAddition added member:@"
-                    + addedMember.getScreenName()
-                    + " listOwner:@" + listOwner.getScreenName()
-                    + " list:" + list.getName());*/
+
         }
 
         @Override
         public void onUserListMemberDeletion(User deletedMember, User listOwner, UserList list) {
-            /*System.out.println("onUserListMemberDeleted deleted member:@"
-                    + deletedMember.getScreenName()
-                    + " listOwner:@" + listOwner.getScreenName()
-                    + " list:" + list.getName());*/
+
         }
 
         @Override
         public void onUserListSubscription(User subscriber, User listOwner, UserList list) {
-            /*System.out.println("onUserListSubscribed subscriber:@"
-                    + subscriber.getScreenName()
-                    + " listOwner:@" + listOwner.getScreenName()
-                    + " list:" + list.getName());*/
+
         }
 
         @Override
         public void onUserListUnsubscription(User subscriber, User listOwner, UserList list) {
-            /*System.out.println("onUserListUnsubscribed subscriber:@"
-                    + subscriber.getScreenName()
-                    + " listOwner:@" + listOwner.getScreenName()
-                    + " list:" + list.getName());*/
+
         }
 
         @Override
         public void onUserListCreation(User listOwner, UserList list) {
-            /*System.out.println("onUserListCreated  listOwner:@"
-                    + listOwner.getScreenName()
-                    + " list:" + list.getName());*/
+
         }
 
         @Override
         public void onUserListUpdate(User listOwner, UserList list) {
-            /*System.out.println("onUserListUpdated  listOwner:@"
-                    + listOwner.getScreenName()
-                    + " list:" + list.getName());*/
+
         }
 
         @Override
         public void onUserListDeletion(User listOwner, UserList list) {
-            /*System.out.println("onUserListDestroyed  listOwner:@"
-                    + listOwner.getScreenName()
-                    + " list:" + list.getName());*/
+
         }
 
         @Override
         public void onUserProfileUpdate(User updatedUser) {
-//            System.out.println("onUserProfileUpdated user:@" + updatedUser.getScreenName());
+//
         }
 
         @Override
         public void onUserDeletion(long deletedUser) {
-//            System.out.println("onUserDeletion user:@" + deletedUser);
+//
         }
 
         @Override
         public void onUserSuspension(long suspendedUser) {
-//            System.out.println("onUserSuspension user:@" + suspendedUser);
+//
         }
 
         @Override
         public void onBlock(User source, User blockedUser) {
-            /*System.out.println("onBlock source:@" + source.getScreenName()
-                    + " target:@" + blockedUser.getScreenName());*/
+
         }
 
         @Override
         public void onUnblock(User source, User unblockedUser) {
-            /*System.out.println("onUnblock source:@" + source.getScreenName()
-                    + " target:@" + unblockedUser.getScreenName());*/
+
         }
 
         @Override
@@ -229,18 +190,20 @@ public final class TwitterStreamReader {
 
         @Override
         public void onFavoritedRetweet(User source, User target, Status favoritedRetweet) {
-            System.out.println("onFavroitedRetweet source:@" + source.getScreenName()
+            /*System.out.println("onFavroitedRetweet source:@" + source.getScreenName()
                     + " target:@" + target.getScreenName()
                     + favoritedRetweet.getUser().getScreenName()
-                    + " - " + favoritedRetweet.getText());
+                    + " - " + favoritedRetweet.getText());*/
         }
 
         @Override
         public void onQuotedTweet(User source, User target, Status quotingTweet) {
-            System.out.println("onQuotedTweet" + source.getScreenName()
-                    + " target:@" + target.getScreenName()
-                    + quotingTweet.getUser().getScreenName()
-                    + " - " + quotingTweet.getText());
+            String topic = quotingTweet.getUser().getName() + " has quoted your status!";
+            try {
+                generateNote(quotingTweet, topic);
+            } catch (IOException|TimeoutException e) {
+                e.printStackTrace();
+            }
         }
 
         @Override
@@ -266,13 +229,9 @@ public final class TwitterStreamReader {
         notificationJSON.put("sourceID", 15);
         notificationJSON.put("time", getCurrentTimeStamp());
         notificationJSON.put("priority", 0);
+        notificationJSON.put("userID", 4);
 
         String stringJSON = notificationJSON.toString();
-        //System.out.println(stringJSON);
-
-        /*PrintWriter writer = new PrintWriter("./src/main/resources/messagetest.json", "UTF-8");
-        writer.println(stringJSON);
-        writer.close();*/
 
         queueJSON = notificationJSON;
         queueString = stringJSON;
@@ -289,13 +248,9 @@ public final class TwitterStreamReader {
         notificationJSON.put("sourceID", 15);
         notificationJSON.put("time", getCurrentTimeStamp());
         notificationJSON.put("priority", 0);
+        notificationJSON.put("userID", 4);
 
         String stringJSON = notificationJSON.toString();
-        //System.out.println(stringJSON);
-
-        /*PrintWriter writer = new PrintWriter("./src/main/resources/messagetest.json", "UTF-8");
-        writer.println(stringJSON);
-        writer.close();*/
 
         queueJSON = notificationJSON;
         queueString = stringJSON;
